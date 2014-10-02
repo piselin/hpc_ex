825c4999a87704b9002ec1d044b036208fc1ea9f
iselinp <piselin@gmail.com>
2014-10-02 19:52:48 +0200

    switched to lambda functions

--- exercises/ex01/integration_parallel.cpp
+++ exercises/ex01/integration_parallel.cpp
@@ -1,21 +1,17 @@
-#include <iostream>
 #include <cmath>
 #include <cassert>
+#include <vector>
+#include <iostream>
 #include <thread>
-#include <future>
+#include <mutex>
 
-
-double func(double x) {
-	return std::sqrt(x)*std::log(x);
-}
-
-// maybe use function object or template instead
-double integrate(std::function<double(double)> fun, double a, double b, unsigned n)
+void integrate(
+	std::function<double(double)> fun, 
+	std::pair<double, std::mutex>& result,
+ 	double a, double b, unsigned n)
 {	
 	assert(n>0);
-
-
-	double S = 0.0;			// the sum
+	double sum = 0.0;		// the sum
 	double dx = (b-a)/n;	// interval double
 	double x = 0.0;			// x* in the formula
 	double x1 = a;			// x_i
@@ -23,32 +19,57 @@ double integrate(std::function<double(double)> fun, double a, double b, unsigned
 	for (int i = 0; i < n; ++i)
 	{
 		x = (x1+x2) / 2;
-		S += fun(x)*dx;
+		sum += fun(x)*dx;
 		x1 = x2;
 		x2 += dx;
 	}
-	return S;
-} 
+	// accuire the lock, it will be released onced the scope is left
+	std::lock_guard<std::mutex> l (result.second);
+ 	result.first += sum;
+}
 
 int main()
 {
 	typedef double bound_t;
-
 	bound_t a = 1;
 	bound_t b = 4;
-	unsigned bins = 10;
+	unsigned const bins = 50;
 
-	//std::size_t const nthreads = std::max(bins, std::thread::hardware_concurrency());
-	std::size_t const nthreads = 2;
+	// setup of the threads
+	std::size_t const nthreads = std::max(1u, std::thread::hardware_concurrency());
+ 	std::vector<std::thread> threads(nthreads);
 
-	std::packaged_task<double()> pt(std::bind(integrate,func, a, a+(b-a)/2., bins/2));
-	std::future<double> fi = pt.get_future();
-	std::thread t (std::move(pt));
+ 	// protect the sum with a mutex
+	std::pair<double, std::mutex> result;
+	result.first=0.;
 
-	double result = integrate(func, a+(b-a)/2., b, bins/2);
+	// the function which we integrate
+	auto f = [] (double x) { return std::sqrt(x)*std::log(x); };
 
-	std::cout << result + fi.get() << std::endl;
-	t.join();
+	// figure out how to distribute the bins
+	int bpt = bins / nthreads; // bins per thread
+	int extrabins = bins%nthreads;
+	
+	double dx = (double)(b-a)/bins;
+	
+	bound_t lowerb = a;
+	bound_t upperb = a;
 
+	for (unsigned i = 0; i < nthreads-1; ++i)
+	{
+		upperb += bpt*dx;
+		// std::cout << "loop " << i << std::endl;
+		// std::cout << "integrate from " << lowerb << " to " << upperb << std::endl;
+		threads[i] = std::thread(integrate,f,std::ref(result),lowerb,upperb,bpt);
+		lowerb = upperb;
+	}
+
+	upperb += (bpt+extrabins)*dx;
+	threads[nthreads-1] = std::thread(integrate,f,std::ref(result),lowerb,upperb,bpt);
+  
+  	for (std::thread& t : threads)
+    	t.join();
+
+    std::cout << result.first << std::endl;
 	return 0;
 }
--- exercises/ex01/integration_seriell.cpp
+++ exercises/ex01/integration_seriell.cpp
@@ -3,11 +3,10 @@
 #include <cassert>
 #include <functional>
 
-
 inline double integrate(std::function<double(double)> fun, double a, double b, unsigned n)
 {	
 	assert(n>0);
-	double S = 0.0;			// the sum
+	double sum = 0.0;			// the sum
 	double dx = (b-a)/n;	// interval size
 	double x = 0.0;			// x* in the formula
 	double x1 = a;			// x_i
@@ -15,23 +14,22 @@ inline double integrate(std::function<double(double)> fun, double a, double b, u
 	for (int i = 0; i < n; ++i)
 	{
 		x = (x1+x2) / 2;
-		S += fun(x)*dx;
+		sum += fun(x)*dx;
 		x1 = x2;
 		x2 += dx;
 	}
-	return S;
+	return sum;
 } 
 
 int main()
 {
 	typedef double bound_t;
-
 	bound_t a = 1;
 	bound_t b = 4;
-	unsigned bins = 100;
+	unsigned bins = 1000;
 
 	// lambda function
 	auto f = [] (double x) { return std::sqrt(x)*std::log(x); };
 
-	std::cout << integrate(f, a,b,bins) << std::endl;
+	std::cout << integrate(f,a,b,bins) << std::endl;
 }
